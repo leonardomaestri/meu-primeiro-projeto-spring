3ba116760d2ebfedfe8856426c1797e1a75f9384
Leonardo Maestri <maestri93@gmail.com>
2019-08-28 20:37:49 -0300

    Add Packages

---
+++ src/main/java/com/unifacef/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.unifacef.enums;
+
+public enum PerfilEnum {
+	 ROLE_ADMIN,
+	 ROLE_USUARIO;
+}
--- src/main/java/com/unifacef/my_first_project_spring/DemoApplication.java
+++ src/main/java/com/unifacef/my_first_project_spring/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.unifacef.my_first_project_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.unifacef.utils.SenhaUtils;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,19 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+		String senhaEncoded =
+		SenhaUtils.gerarBCrypt("123456");
+		System.out.println("Senha encoded: " +
+		senhaEncoded);
+		senhaEncoded =
+		SenhaUtils.gerarBCrypt("123456");
+		System.out.println("Senha encoded novamente:" + senhaEncoded);
+		System.out.println("Senha válida: " +
+		SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
 }
